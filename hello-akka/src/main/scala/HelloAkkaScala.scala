b176a04146a932d9f61ea878947e1126ad959ce5
Milad Khajavi <khajavi@gmail.com>
2016-06-05 20:56:26 +0430

    add actor with argument

--- hello-akka/src/main/scala/HelloAkkaScala.scala
+++ hello-akka/src/main/scala/HelloAkkaScala.scala
@@ -3,16 +3,16 @@ import akka.actor.{Actor, ActorSystem, Props}
 /**
   * Example from http://alvinalexander.com/scala/simple-scala-akka-actor-examples-hello-world-actors
   */
-class HelloActor extends Actor {
+class HelloActor(name: String) extends Actor {
   def receive = {
-    case "hello" => println("hello back at you")
-    case _ => println("huh?")
+    case "hello" => println(s"${name}: hello back at you")
+    case _ => println(s"${name}: huh?")
   }
 }
 
 object Main extends App {
   val system = ActorSystem("HelloSystem")
-  val helloActor = system.actorOf(Props[HelloActor], name = "helloactor")
+  val helloActor = system.actorOf(Props(new HelloActor("Ali")), name = "helloactor")
 
   helloActor ! "hello"
   helloActor ! "salam"

7fad4b8be11046bd722bb5a2dd8368f326c153ea
Milad Khajavi <khajavi@gmail.com>
2016-07-14 02:28:04 +0430

    add publish subscribe pattern

---
+++ publish-subscribe/src/main/scala/Main.scala
@@ -0,0 +1,67 @@
+
+object Observers extends App {
+
+  val a = new BankAccount
+  val b = new BankAccount
+
+  val c = new Consolidator(List(a, b))
+
+  println(c totalBalance)
+
+  a deposit 20
+  println(c totalBalance)
+
+  b deposit 30
+  println(c totalBalance)
+
+}
+
+trait Publisher {
+  private var subscribers: Set[Subscriber] = Set()
+
+  def subscribe(subscriber: Subscriber): Unit =
+    subscribers += subscriber
+
+  def unsubscribe(subscriber: Subscriber): Unit =
+    subscribers -= subscriber
+
+  def publish(): Unit =
+    subscribers foreach (_.handler(this))
+}
+
+trait Subscriber {
+  def handler(pub: Publisher)
+}
+
+class BankAccount extends Publisher {
+  private var balance = 0
+
+  def currentBalance = balance
+
+  def deposit(amount: Int): Unit =
+    if (amount > 0) {
+      balance = balance + amount
+      publish()
+    }
+
+  def withdraw(amount: Int): Unit =
+    if (0 < amount && amount <= balance) {
+      balance = balance - amount
+      publish()
+    } else
+      throw new Error("insufficient found")
+}
+
+class Consolidator(observed: List[BankAccount]) extends Subscriber {
+  observed foreach (_.subscribe(this))
+
+  private var total: Int = _
+  compute()
+
+  private def compute() =
+    total = observed.map(_.currentBalance).sum
+
+  def handler(publisher: Publisher) = compute()
+
+  def totalBalance = total
+}

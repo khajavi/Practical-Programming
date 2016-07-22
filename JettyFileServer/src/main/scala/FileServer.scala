0eb70c390a98e4757309aaad848d7b3d61f67d0d
Milad Khajavi <khajavi@gmail.com>
2016-07-22 17:08:44 +0430

    refactor

--- JettyFileServer/src/main/scala/FileServer.scala
+++ JettyFileServer/src/main/scala/FileServer.scala
@@ -2,18 +2,19 @@ import org.eclipse.jetty.server.handler.{DefaultHandler, HandlerList, ResourceHa
 import org.eclipse.jetty.server.{Handler, Server, _}
 
 object FileServer extends App {
-  val server: Server = new Server(8080)
+  val server = new Server(8080)
   // Create the ResourceHandler. It is the object that will actually handle the request for a given file. It is
   // a Jetty Handler object so it is suitable for chaining with other handlers as you will see in other examples.
-  val resource_handler: ResourceHandler = new ResourceHandler
+  val resource_handler = new ResourceHandler
   // Configure the ResourceHandler. Setting the resource base indicates where the files should be served out of.
   // In this example it is the current directory but it can be configured to anything that the jvm has access to.
   resource_handler.setDirectoriesListed(true)
   resource_handler.setWelcomeFiles(Array[String]("index.html"))
   resource_handler.setResourceBase("src/main/resources/")
   // Add the ResourceHandler to the server.
-  val handlers: HandlerList = new HandlerList
-  handlers.setHandlers(Array[Handler](resource_handler, new DefaultHandler))
+  val handlers = new HandlerList
+  handlers.addHandler(resource_handler)
+  handlers.addHandler(new DefaultHandler)
   server.setHandler(handlers)
 
   // Start things up! By using the server.join() the server thread will join with the current thread.
